1d73cb9ba1df0f51760251356591dbf59d554127
Abhishek Desai <thisisabhishekdesai@gmail.com>
2022-12-11 12:16:18 +0530

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  // title = 'todolist';
+
+  //Multiple Select Value:
+  cars = [
+    { id: 1, name: "BMW Hyundai" },
+    { id: 2, name: "Kia Tata" },
+    { id: 3, name: "Volkswagen Ford" },
+    { id: 4, name: "Renault Audi" },
+    { id: 5, name: "Mercedes Benz Skoda" },
+  ];
+  
+  selected:any = [{ id: 3, name: "Volkswagen Ford" }];
+
+  ButtonClick(){
+    console.log(this.selected);
+  }
+
+}
+
+//Style.css File in angular
+
+
